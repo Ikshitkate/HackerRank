42ef832d21d581ba6f7b34020511a56bd6ff0b3c
Dewansh Khandelwal <dewansh3255@gmail.com>
2023-10-01 16:42:06 +0530

    Update Between two sets.cpp
    It is very important to keep your code efficient, so you can try this method of binary search to optimize your code

--- Algorithms/Implementation/Between two sets.cpp
+++ Algorithms/Implementation/Between two sets.cpp
@@ -11,8 +11,22 @@ int main()
     int b[m];
     for (int i = 0; i < m; i++)
         cin>>b[i];
+    int binarySearch(int arr[], int l, int r, int x)
+    {
+	while (l <= r) {
+		int m = l + (r - l) / 2;
+		if (arr[m] == x)
+			return m;
+		if (arr[m] < x)
+			l = m + 1;
+		else
+			r = m - 1;
+	}
+	return -1;
+}
 
-    for(int i=1; i<=100; i++)
+    int i;
+    for(i=1; i<=100; i++)
     {
         p=true;
         for(int j=0; j<n; j++)
