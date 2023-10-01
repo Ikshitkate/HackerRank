4d183e241f249410429b8b19997f5cd526e789a6
Ikshitkate <93122683+Ikshitkate@users.noreply.github.com>
2023-10-01 16:56:04 +0530

    Update Solve Me First.cpp

--- Algorithms/Warmup/Solve Me First.cpp
+++ Algorithms/Warmup/Solve Me First.cpp
@@ -6,15 +6,17 @@
 using namespace std;
 
 int solveMeFirst(int a, int b) {
- // Hint: Type return a+b; below:
-  return a+b;
+// return a+b here, to get the sesired results
+ return a+b;
 }
 
 int main() {
-  int num1, num2;
+  int num1;
+ int num2;
   int sum;
-  cin>>num1>>num2;
+  cin>>num1;
+ cin>>num2;
   sum = solveMeFirst(num1,num2);
-  cout<<sum;
+  printf("%d", sum);
   return 0;
 }
